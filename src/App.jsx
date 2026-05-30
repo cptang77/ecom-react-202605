d1eb640dbd1ebcfdf167b848071a1e32e4c76037
cptang77 <chinptang3-c@my.cityu.edu.hk>
2026-05-30 17:09:06 +0800

    add auth

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,26 @@
-import './App.css'
-import {Routes, Route} from 'react-router-dom'
-import Home from './pages/Home.jsx'
-import Auth from './pages/Auth.jsx'
-import Checkout from './pages/checkout.jsx'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import Auth from "./pages/Auth.jsx";
+import Checkout from "./pages/checkout.jsx";
 // import Products from './pages/Products.jsx'
-import Navbar from './components/Navbar.jsx'
+import Navbar from "./components/Navbar.jsx";
+import AuthProvider from "./context/AuthContext.jsx";
 
 function App() {
-
   return (
-    <div className='app'>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/auth' element={<Auth />} />
-        <Route path='/checkout' element={<Checkout />} />
-        {/* <Route path='/products' element={Products} /> */}
-      </Routes>
-    </div>
-  )
+    <AuthProvider>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/checkout" element={<Checkout />} />
+          {/* <Route path='/products' element={Products} /> */}
+        </Routes>
+      </div>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
